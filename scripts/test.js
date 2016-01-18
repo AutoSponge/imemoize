7e5a9a53444045e82277209de592410a0f9967d9
Paul Grenier <pgrenier@gmail.com>
2016-01-18 15:16:14 -0500

    adds test.js script

---
+++ scripts/test.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env babel-node
+import {spawn} from 'child_process'
+
+const [,, coverage] = process.argv
+
+const coverageOutputDir = 'coverage'
+const stdio = 'inherit'
+const mocha = './node_modules/.bin/_mocha'
+const babel = './node_modules/.bin/babel-node'
+const mochaParams = ['--compilers', 'js:babel-register']
+const mochaReport = ['--reporter', 'spec']
+const covParams = ['node_modules/.bin/isparta', 'cover', '--dir', coverageOutputDir, '--report', 'text', '--report', 'html']
+
+if (coverage) {
+  spawn(babel, [...covParams, mocha, ...mochaParams], {stdio})
+} else {
+  spawn(mocha, [...mochaParams, ...mochaReport], {stdio})
+}
